980d2fa7565ca84031de22b0f4e744628dd2ed55
DaffaKR <daffaquanta@gmail.com>
2022-09-18 19:05:36 +0700

    1 space for input

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from math_function import add, div, mul
 
 def main():
 
-    data_1 = int(input("masukkan input 1 :"))
-    data_2 = int(input("masukkan input 2 :"))
-    operator = input("masukkan operator :")
+    data_1 = int(input("masukkan input 1 : "))
+    data_2 = int(input("masukkan input 2 : "))
+    operator = input("masukkan operator : ")
 
     if operator == "+":
         result = add(data_1, data_2)
